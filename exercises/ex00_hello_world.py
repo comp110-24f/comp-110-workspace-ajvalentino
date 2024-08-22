f372fcf116e4c75c8228126abe84e4bb0d39a8bf
Anthony Valentino <97629772+ajvalentino@users.noreply.github.com>
2024-08-22 11:36:23 -0400

    EX00 Refresh

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,8 @@
+"""My first exercise in COMP110!"""
+
+__author__ = "123456789"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
